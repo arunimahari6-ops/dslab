58ba5b0d87e6ebb1516cd9b8b6122846a0a4695e
arunimahari6-ops <arunimahari6@gmail.com>
2026-09-08 11:23:05 +0530

    Add files via upload

---
+++ binary.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+int a[10],n,key,low,high,mid,i;
+printf("enter number of elements:");
+scanf("%d", &n);
+printf("enter the sorted elements");
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+printf("entrer the elements to search");
+scanf("%d",&key);
+low=0;
+high=n-1;
+while(low<=high)
+{
+mid=(low+high)/2;
+if(a[mid]==key)
+{
+printf("element  found at position %d",mid+1);
+return 0;
+}
+else if(key<a[mid])
+high=mid-1;
+else
+low=mid+1;
+}
+printf("element not found");
+return 0;
+}
